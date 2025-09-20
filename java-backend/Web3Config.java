2e36b403a49e92bb9a1aed8ee5de1232e8a82490
Saurabh vishwakarma <saurvishwakarma040207@gmail.com>
2025-09-20 20:42:16 +0530

    Update Web3Config.java

--- java-backend/Web3Config.java
+++ java-backend/Web3Config.java
@@ -1,1 +1,19 @@
 
+package com.project.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+
+/**
+ * Web3 Configuration Class
+ * Sets up blockchain connection and Web3j instance
+ */
+@Configuration
+public class Web3Config {
+    
+    private static final Logger logger = LoggerFactory.getLogger(Web3Config.class);
