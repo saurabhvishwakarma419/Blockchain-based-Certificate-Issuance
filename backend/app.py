385246ee670be241f05c6df0f252b366534003d9
Saurabh Vishwakarma <saurvishwakarma040207@gmail.com>
2026-04-29 18:58:05 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from utils.hashing import generate_certificate_hash
 
+app = Flask(__name__)
+CORS(app)  # Taaki Frontend bina kisi error ke connect ho sake
+
+@app.route('/generate-hash', methods=['POST'])
+def get_hash():
+    data = request.json
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    
+    # Hash generate karo
+    cert_hash = generate_certificate_hash(data)
+    
+    return jsonify({
+        "status": "success",
+        "certificate_hash": cert_hash,
+        "message": "Hash generated for Blockchain issuance"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
