9905845f5124e3b3c391a32636c5f40e277f677c
Saurabh Vishwakarma <saurvishwakarma040207@gmail.com>
2026-04-29 19:03:41 +0530

    Update Navbar.js

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,1 +1,9 @@
+import React from 'react';
 
+const Navbar = () => (
+    <nav style={{ background: '#282c34', padding: '1rem', color: 'white' }}>
+        <h2>BlockCert Verify</h2>
+    </nav>
+);
+
+export default Navbar;
